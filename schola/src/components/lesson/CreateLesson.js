697613f1a10d2811a67011a7dd28051f054d4be6
Renan Barreto Garcia <renan.garcia@reitoria.unicamp.br>
2019-10-16 15:19:00 -0300

    create lesson

--- schola/src/components/lesson/CreateLesson.js
+++ schola/src/components/lesson/CreateLesson.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import firebase from '../../firebase';
 import { connect } from 'react-redux'; 
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTimesCircle } from '@fortawesome/free-solid-svg-icons'
+import { alertbox } from '../utils/alert';
 
 class CreateLesson extends React.Component {
 
@@ -12,11 +12,15 @@ class CreateLesson extends React.Component {
         super(props)
         this.handleFileInput = this.handleFileInput.bind(this);
         this.uploadFiles = this.uploadFiles.bind(this);
-        this.renderProgress = this.renderProgress.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     state = {
-        fileList: []
+        fileList: [],
+        titleInput: '',
+        ageInput: '',
+        disciplineInput: '',
+        descriptionInput: ''
     }
 
     handleFileInput() {
@@ -27,21 +31,47 @@ class CreateLesson extends React.Component {
         })
     }
 
-    handleFormInput() {
-        
+    handleTitleInput(e) {
+        this.setState({ titleInput: e.target.value});
+        console.log(this.state.titleInput);
+    }
+
+    handleAgeInput(e) {
+        this.setState({ ageInput: e.target.value});
+        console.log(this.state.ageInput);
+    }
+
+    handleDisciplineInput(e) {
+        this.setState({ disciplineInput: e.currentTarget.value});
+        console.log(this.state.disciplineInput);
+    }
+
+    handleDescriptionInput(e) {
+        this.setState({ descriptionInput: e.target.value});
+        console.log(this.state.descriptionInput);
     }
 
     convertItemsToArray(array) {
         return Array.from(array)
     }
 
+    /**
+     * Remove um item da lista de upload
+     * @param {*} i 
+     */
     deleteListItem(i) {
         let newState = this.state.fileList;
         newState.splice(i, 1);
         this.setState({ fileList: newState});
     }
 
-    uploadFiles() {
+    /**
+     * 
+     */
+     uploadFiles() {
+        const self = this;
+        let db = firebase.firestore();
+        let docRef = db.collection(`users/${this.props.userObject.uid}/lessons`).doc(this.state.titleInput);
         for (let file of this.state.fileList) {
             let storageRef = firebase.storage().ref(`${this.props.userObject.uid}/lessons/${file.name}`);
             let uploadTask = storageRef.put(file);
@@ -49,67 +79,125 @@ class CreateLesson extends React.Component {
                 let progress = Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
                 //Render a informação no tiem da lista
                 document.getElementById(file.name).innerHTML = `Progresso: ${progress}%`;
-                switch (snapshot.state) {
-                  case firebase.storage.TaskState.PAUSED:
-                    console.log('Upload is paused');
-                    break;
-                  case firebase.storage.TaskState.RUNNING:
-                    console.log('Upload is running');
-                    break;
-                }
+                // switch (snapshot.state) {
+                //   case firebase.storage.TaskState.PAUSED:
+                //     console.log('Upload is paused');
+                //     break;
+                //   case firebase.storage.TaskState.RUNNING:
+                //     console.log('Upload is running');
+                //     break;
+                // }
             }, function(error) {
-            console.log("Ocorreu um erro: " + error);
+                console.log("Ocorreu um erro: " + error);
+                throw error;
             }, function() {
-            uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
-                console.log('File available at', downloadURL);
-            });
+            uploadTask.snapshot.ref.getDownloadURL()
+                .then(function(downloadURL) {
+                    console.log('File available at', downloadURL);
+                    let updatedArray = firebase.firestore.FieldValue.arrayUnion(downloadURL)
+                    docRef.update({
+                        filesURLs: updatedArray
+                    })
+                    self.deleteListItem();
+                });
             });
         }
+        return true
     }
 
+    /**
+     * Insere no banco as informações da lesson, incluindo urls dos arquivos
+     * @param {[uploadTask]} urls 
+     */
+    sendLessonInfo() {
+        try {
+            let db = firebase.firestore();
+            let docRef = db.collection(`users/${this.props.userObject.uid}/lessons`).doc(this.state.titleInput);
+    
+            docRef.set({
+                title: this.state.titleInput,
+                targetAge: this.state.ageInput,
+                discipline: this.state.disciplineInput,
+                desc: this.state.descriptionInput
+            });
+        } catch(error) {
+            console.log(error);
+        }
+        return true
+    }
+
+    //era para renderizar o progesso do upload no component - não está sendo usado
     renderProgress(id, progress) {
         document.getElementById(id).innerHTML = `Completado: ${progress}`;
     }
 
-    componentWillMount() {
-        let storageRef = firebase.storage().ref(`${this.props.userObject.uid}/lessons`);
-        storageRef.listAll().then(function(res) {
-            res.items.forEach(function(itemRef) {
-                itemRef.getDownloadURL().then(url => console.log(url));
-              });
-        })
+    handleSubmit() {
+        if (this.state.titleInput &&
+            this.state.ageInput &&
+            this.state.disciplineInput &&
+            this.state.descriptionInput !== '') {
+            try {
+                this.uploadFiles();
+                this.sendLessonInfo();
+            } catch(err) {
+                console.log(err);
+            }
+            alertbox.show('Lição cadastrada!');
+        } else {
+            alertbox.show('Preencha todos os campos corretamente.')
+        }
     }
 
+    // componentWillMount() {
+    //     let storageRef = firebase.storage().ref(`${this.props.userObject.uid}/lessons`);
+    //     storageRef.listAll().then(function(res) {
+    //         res.items.forEach(function(itemRef) {
+    //             itemRef.getDownloadURL().then(url => console.log(url));
+    //           });
+    //     })
+    // }
+
     render() {
-        console.log(this.state.fileList);
-        console.log(this.props);
         return (
             <div>
                 <div className="row row-center">
                     <h3>Create lesson</h3>
                 </div>
                 <label for="titulo">Título</label>
-                <input type="text" name="titulo" id="titulo"/>
-                <div className="row row-center">
-                    <div className="column column-25">
+                <input onChange={e => this.handleTitleInput(e)}
+                        type="text"
+                        name="titulo"
+                        id="title"/>
+                <div className="row">
+                    <div className="column column-25 mobile-full-width">
                         <label for="idade">Idade alvo</label>
-                        <input type="number" name="idade" id="idade" min="0" max="18"/>
+                        <input onChange={e => this.handleAgeInput(e)}
+                                type="number"
+                                name="idade"
+                                id="age"
+                                min="0"
+                                max="18"/>
                     </div>
-                    <div className="column column-75">
+                    <div className="column column-75 mobile-full-width">
                         <label for="titulo">Disciplina</label>
-                        <select>
-                            <option>Matemática</option>
-                            <option>Gramática</option>
-                            <option>Inglês</option>
+                        <select onChange={(e) => this.handleDisciplineInput(e)}id="discipline">
+                            <option value="math">Matemática</option>
+                            <option value="grammar">Gramática</option>
+                            <option value="english">Inglês</option>
                         </select>
                     </div>
                 </div>
-                <textarea id="description" name="description">
-                    Descreva o material
+                <textarea onChange={e => this.handleDescriptionInput(e)}
+                            id="description"
+                            name="description"
+                            defaultValue="Descreva o material">
                 </textarea>
                 <div className="row row-center">
                     <label className="input-file-label" for="lesson-file">Carregar arquivos</label>
-                    <input onChange={this.handleFileInput} type="file" id="lesson-file" multiple/>
+                    <input onChange={this.handleFileInput}
+                            type="file"
+                            id="lesson-file"
+                            multiple/>
                 </div>
                 <div className="row row-center">
                     <ul className="file-list">
@@ -125,7 +213,7 @@ class CreateLesson extends React.Component {
                     </ul>
                 </div>
                 <div>
-                    <button onClick={this.uploadFiles}>Enviar</button>
+                    <button onClick={this.handleSubmit}>Enviar</button>
                 </div>
             </div>
         )
@@ -138,7 +226,7 @@ const mapStateToProps = (store) => ({
     isLogged: store.authReducer.isLogged
   });
 
-  const mapDispatchToProps = dispatch => ({
-  })
+const mapDispatchToProps = dispatch => ({
+})
 
 export default connect(mapStateToProps, mapDispatchToProps)(CreateLesson)
